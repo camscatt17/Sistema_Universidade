2fcdaf1094f9f11c35cb8b7fc96c24c643552be4
Ellejeane Camila Santos <camscatt17@gmail.com>
2023-02-06 18:15:07 -0300

    Introdução ao C++

---
+++ main.cpp
@@ -0,0 +1,48 @@
+//Apesar de conter referência escondida suportada apenas pelo C++, ainda é um programa procedimental 
+#include <stdio.h>
+
+struct Pessoa{
+    int dia;
+    int mes;
+    int ano;
+    int idade;
+};
+
+void Calculo_Idade(struct Pessoa &p, int dia, int mes, int ano){
+    p.idade = ano - p.mes;
+    
+    if(p.mes > mes){
+        p.idade-=1;
+    }else{
+        if(p.mes == mes){
+            if(p.dia > dia){
+                p.idade-=1;
+            }    
+        }
+    }
+}
+
+
+int main(){
+    struct Pessoa Einstein, Newton;
+
+    Einstein.dia = 14;
+    Einstein.mes = 3;
+    Einstein.ano = 1879;
+    Einstein.idade = -1;
+
+    Newton.dia = 4;
+    Newton.mes = 1;
+    Newton.ano = 1643;
+    Newton.idade = -1;
+
+    Calculo_Idade(Einstein, 6, 2, 2023);
+    Calculo_Idade(Newton, 6, 2, 2023);
+
+    printf("A idade de Einstein seria %d \n", Einstein.idade);
+    printf("A idade de Newton seria %d \n", Newton.idade);
+
+    getchar();
+    return 0;
+}
+
