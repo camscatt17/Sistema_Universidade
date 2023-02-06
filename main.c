8a2b68be82f816c062dba1dfecd99373661476ee
Ellejeane Camila Santos <camscatt17@gmail.com>
2023-02-06 17:29:32 -0300

    Exemplo de Código em C - V2

--- main.c
+++ main.c
@@ -7,10 +7,26 @@ struct Pessoa{
     int idade;
 };
 
-int Calculo_Idade(struct Pessoa p, int ano){
+/*int Calculo_Idade(struct Pessoa p, int ano){
     int idade = ano - p.ano;
     return idade;
+}*/
+
+int Calculo_Idade(struct Pessoa p, int dia, int mes, int ano){
+    int idade = ano - p.ano;
+    
+    if(p.mes > mes){
+        idade-=1;
+    }else{
+        if(p.mes == mes){
+            if(p.dia > dia){
+                idade-=1;
+            }    
+        }
+    }
+    return idade;
 }
+
 
 int main(){
     struct Pessoa Einstein, Newton;
@@ -23,8 +39,8 @@ int main(){
     Newton.mes = 1;
     Newton.ano = 1643;
 
-    Einstein.idade = Calculo_Idade(Einstein, 2023);
-    Newton.idade = Calculo_Idade(Newton, 2023);
+    Einstein.idade = Calculo_Idade(Einstein, 6, 2, 2023);
+    Newton.idade = Calculo_Idade(Newton, 6, 2, 2023);
 
     printf("A idade de Einstein seria %d \n", Einstein.idade);
     printf("A idade de Newton seria %d \n", Newton.idade);
