51b258332136045423c28f4b4044d23af342539b
Ellejeane Camila Santos <camscatt17@gmail.com>
2023-02-07 15:09:24 -0300

    Introdução ao POO: Função Construtora

--- main.cpp
+++ main.cpp
@@ -1,47 +1,49 @@
-//Apesar de conter referência escondida suportada apenas pelo C++, ainda é um programa procedimental 
 #include <stdio.h>
 
 struct Pessoa{
-    int dia;
-    int mes;
-    int ano;
-    int idade;
-};
+    public://Identifica a visibilidade dos atributos e funções
+    //Como esses atributos são públicos, conseguem ser acessados fora da struct
+        int dia;
+        int mes;
+        int ano;
+        int idade;
 
-void Calculo_Idade(struct Pessoa &p, int dia, int mes, int ano){
-    p.idade = ano - p.mes;
+    //Função Construtora: Inicializa variáveis
+    //Vantagem: O código fica mais limpo
+    Pessoa (int dia, int mes, int ano){
+        this->dia = dia;
+        this->mes = mes;
+        this->ano = ano;
+        this->idade = -1;
+    }
+
+    //A struct além de armazenar os dados, armazena também a função, mais de acordo com os princípios da POO
+    void Calculo_Idade(int dia, int mes, int ano){
+        this->idade = ano - this->ano;
     
-    if(p.mes > mes){
-        p.idade-=1;
-    }else{
-        if(p.mes == mes){
-            if(p.dia > dia){
-                p.idade-=1;
-            }    
+        if(this->mes > mes){
+            this->idade-=1;
+        }else{
+            if(this->mes == mes){
+                if(this->dia > dia){
+                    this->idade-=1;
+                }    
+            }
         }
     }
-}
+};
 
 
 int main(){
-    struct Pessoa Einstein, Newton;
+    Pessoa Einstein (14, 3, 1879); //Além de criar um objeto do tipo Pessoa, já se inicializa esse objeto através da sua construtora
+    Pessoa Newton(4, 1, 1643);
 
-    Einstein.dia = 14;
-    Einstein.mes = 3;
-    Einstein.ano = 1879;
-    Einstein.idade = -1;
-
-    Newton.dia = 4;
-    Newton.mes = 1;
-    Newton.ano = 1643;
-    Newton.idade = -1;
-
-    Calculo_Idade(Einstein, 6, 2, 2023);
-    Calculo_Idade(Newton, 6, 2, 2023);
+    Einstein.Calculo_Idade(6, 2, 2023);
+    Newton.Calculo_Idade(6, 2, 2023);
 
     printf("A idade de Einstein seria %d \n", Einstein.idade);
     printf("A idade de Newton seria %d \n", Newton.idade);
-
+    
     getchar();
     return 0;
 }
